7b7ab09ce27be939a144b24f7b3617d54c4a730b
Remotar-10 <remotar@gmail.com>
2026-01-12 14:50:47 -0300

    🧹 Remove avatar "U" e tagline "Territorial Mastery"
    - Remove avatar do canto direito
    - Remove subtitle "Territorial Mastery"
    - Navbar apenas com logo e nome

--- components/navbar.js
+++ components/navbar.js
@@ -18,14 +18,6 @@ class CustomNavbar extends HTMLElement {
                         </div>
                         <div>
                             <h1 class="text-xl font-bold text-gray-900">GeoClient SP</h1>
-                            <p class="text-xs text-gray-500">Territorial Mastery</p>
-                        </div>
-                    </div>
-                    
-                    <div class="flex items-center gap-4">
-                        <!-- Avatar do usuário -->
-                        <div class="w-10 h-10 bg-gray-300 rounded-full flex items-center justify-center text-gray-700 font-bold cursor-pointer hover:bg-gray-400 transition" title="Perfil">
-                            U
                         </div>
                     </div>
                 </div>
